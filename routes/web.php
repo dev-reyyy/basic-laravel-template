d9ccccd57b8f9737e8f7d469ef259a308645a63f
rey <dev.reyyy@gmail.com>
2025-07-19 00:09:17 +0800

    Modify routes, typograhpy setup, and added page title.

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ Route::middleware(['guest'])->group(function () {
     Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 
     Route::get('/login', [LoginController::class, 'login'])->name('login');
-    Route::post('/login', [LoginController::class, 'authenticate'])->name('authenticate');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'mainDashboard'])->name('dashboard.main');
 
-    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('login.logout');
 });
